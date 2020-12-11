7429a46a61c5fd9cb8c948706adf02133aeb60b8
DaniloSant03 <75862223+DaniloSant03@users.noreply.github.com>
2020-12-11 18:28:00 -0300

    Add files via upload

---
+++ Exercício 3/Exercício 3/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_3
+{
+    class Quadrado
+    {
+        #region Atributos
+        double diagonal;
+        double area;
+        #endregion
+
+        #region Construtores
+        public Quadrado()
+        {
+            this.diagonal = 0;
+            this.area = 0;
+        }
+
+        public Quadrado(double diagonal)
+        {
+            this.diagonal = diagonal;
+            this.area = 0;
+        }
+        #endregion
+
+        #region Setters e Getters
+        public void SetDiagonal(double diagonal)
+        {
+            this.diagonal = diagonal;
+        }
+
+        public double GetDiagonal()
+        {
+            return this.diagonal;
+        }
+
+        public double GetArea()
+        {
+            return this.area;
+        }
+        #endregion
+
+        #region Métodos funcionais
+        public double CalcularArea()
+        {
+            this.area = Math.Pow(this.diagonal, 2)/2;
+            return this.area;
+        }
+
+        public double CalcularArea(double diagonal)
+        {
+            this.area = Math.Pow(diagonal, 2) / 2;
+            return this.area;
+        }
+        #endregion
+    }
+}
